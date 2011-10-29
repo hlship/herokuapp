ca43361af581bcb5f81ec7d02b14a5ece3832013
Howard M. Lewis Ship <hlship@gmail.com>
2011-10-28 17:06:11 -0700

    In development mode, enable access to @WhitelistOnly protected pages from anywhere

--- src/main/java/org/example/herokuapp/services/DevelopmentModule.java
+++ src/main/java/org/example/herokuapp/services/DevelopmentModule.java
@@ -1,27 +1,20 @@
 package org.example.herokuapp.services;
 
-import java.io.IOException;
-
-import org.apache.tapestry5.*;
+import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
-import org.apache.tapestry5.ioc.ServiceBinder;
-import org.apache.tapestry5.ioc.annotations.Local;
+import org.apache.tapestry5.ioc.annotations.Contribute;
 import org.apache.tapestry5.services.Request;
-import org.apache.tapestry5.services.RequestFilter;
-import org.apache.tapestry5.services.RequestHandler;
-import org.apache.tapestry5.services.Response;
-import org.slf4j.Logger;
+import org.apache.tapestry5.services.security.ClientWhitelist;
+import org.apache.tapestry5.services.security.WhitelistAnalyzer;
 
 /**
  * This module is automatically included as part of the Tapestry IoC Registry if <em>tapestry.execution-mode</em>
  * includes <code>development</code>.
  */
-public class DevelopmentModule
-{
+public class DevelopmentModule {
     public static void contributeApplicationDefaults(
-            MappedConfiguration<String, Object> configuration)
-    {
+            MappedConfiguration<String, Object> configuration) {
         // The factory default is true but during the early stages of an application
         // overriding to false is a good idea. In addition, this is often overridden
         // on the command line as -Dtapestry.production-mode=false
@@ -33,4 +26,16 @@ public class DevelopmentModule
         // change, to force the browser to download new versions.
         configuration.add(SymbolConstants.APPLICATION_VERSION, "1.0-SNAPSHOT-DEV");
     }
+
+    @Contribute(ClientWhitelist.class)
+    public void enableWhitelistAccessInDevelopmentMode(OrderedConfiguration<WhitelistAnalyzer> configuration) {
+        configuration.add("DevAccessAlways",
+                new WhitelistAnalyzer() {
+                    public boolean isRequestOnWhitelist(Request request) {
+                        return true;
+                    }
+                }
+        );
+    }
+
 }
